33f6468d6572c32c7f4ce6d18dd43f1b1ad7d90d
Sɪɢᴍᴀ Tʜᴀᴋᴜʀ 👀 <reetsingh3666@gmail.com>
2026-08-06 08:25:52 +0530

    Update and rename thumbnails (1) (5).py to thumbnails.py

--- VaishuMusic/utils/thumbnails.py
+++ VaishuMusic/utils/thumbnails.py
@@ -16,7 +16,7 @@ import os, aiofiles, aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from py_yt import VideosSearch
 from config import YOUTUBE_IMG_URL
-from ShashankMusic import app
+from VaishuMusic import app
 
 CACHE_DIR = "cache"
 os.makedirs(CACHE_DIR, exist_ok=True)
@@ -76,8 +76,8 @@ async def get_thumb(videoid: str, player_username: str = None) -> str:
     draw = ImageDraw.Draw(bg)
 
     try:
-        font_bold = "ShashankMusic/assets/font2.ttf"
-        font_med = "ShashankMusic/assets/font.ttf"
+        font_bold = "VaishuMusic/assets/font2.ttf"
+        font_med = "VaishuMusic/assets/font.ttf"
         title_font = ImageFont.truetype(font_bold, 60)
         artist_font = ImageFont.truetype(font_med, 40)
         time_font = ImageFont.truetype(font_med, 32)
